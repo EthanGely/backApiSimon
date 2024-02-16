c7886b0a949fd55b131d64f4ed6aaa78e8986332
Ethan <ethangely@free.fr>
2024-02-16 13:43:16 +0100

    init api date simon

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {createServer} from 'node:http';
+import cors from 'cors';
+
+const app = express();
+const server = createServer(app);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(Date.now().toString());
+});
+
+
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
